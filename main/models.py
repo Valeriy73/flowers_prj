b5465777455825bbd42ceae22d0333921951121b
Valeriy73 <turlera@ukr.net>
2017-02-24 18:16:57 +0200

    model first

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
+# -*- encoding: utf-8 -*- 
 from __future__ import unicode_literals
-
 from django.db import models
 
 
@@ -7,6 +7,15 @@ from django.db import models
 class Item(models.Model):
     """ Item Model """
 
-    class Meta(object):
-        verbose_name = u"Карточка товара"
-        verbose_name_plural = u"Карточки товаров"
+    #class Meta(object):
+    #    verbose_name = u"Карточка товара"
+    #    verbose_name_plural = u"Карточки товаров"
+
+    title = models.CharField(max_length=255, verbose_name='Название букета')
+    description = models.TextField(verbose_name="Описание букета")
+    price = models.IntegerField(default=0, verbose_name="Цена")
+    image = models.TextField(verbose_name="Ссылка на изображение")
+
+    def __str__(self):
+        return 'Букет %s' % self.title
+
